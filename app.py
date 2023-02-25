38093cd5bccacaf0df7cbf12ba8302e5e2c64c63
Kate Johnson <johnson.kat.g@gmail.com>
2023-02-24 21:29:25 -0500

    working form submission and display of pets from db on homepage

--- app.py
+++ app.py
@@ -1,17 +1,21 @@
-from flask import (render_template, url_for, request)
+from flask import (render_template, url_for, request, redirect)
 from models import db, Pet, app
-
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    pets = Pet.query.all()
+    return render_template('index.html', pets=pets)
 
 
 @app.route('/add-pet', methods=['GET', 'POST'])
 def add_pet():
-    print(request.form)
-    print(request.form['name'])
+    if request.form:  # If form is submitted after POST
+        new_pet = Pet(name=request.form['name'], age=request.form['age'], breed=request.form
+                      ['breed'], color=request.form['color'], size=request.form['size'], weight=request.form['weight'], url=request.form['url'], url_tag=request.form['alt'], pet_type=request.form['pet'], gender=request.form['gender'], spay=request.form['spay'], house_trained=request.form['housetrained'], description=request.form['description'])
+        db.session.add(new_pet)
+        db.session.commit()
+        return redirect(url_for('index'))
     return render_template('addpet.html')
 
 
@@ -21,6 +25,9 @@ def pet():
 
 
 if __name__ == '__main__':
+    app.run(debug=True, port=8000, host='127.0.0.1')
+    
     with app.app_context():
         db.create_all()
-    app.run(debug=True, port=8000, host='127.0.0.1')
+        
+    
